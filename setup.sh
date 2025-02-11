d2ae94a79de7741c23c2fd3f4365a4decb05af6f
Barinderpreet Singh <naffydharni006@gmail.com>
2025-02-11 14:32:04 +0530

    Add ghostty config to dotfiles

--- setup.sh
+++ setup.sh
@@ -146,6 +146,7 @@ link_dotfiles() {
     create_symlink "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
     create_symlink "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
     create_symlink "$DOTFILES_DIR/.config/gh" "$HOME/.config/gh"
+    create_symlink "$DOTFILES_DIR/.config/ghostty" "$HOME/.config/ghostty"
     create_symlink "$DOTFILES_DIR/.config/qutebrowser" "$HOME/.config/qutebrowser"
     create_symlink "$DOTFILES_DIR/.zsh_functions" "$HOME/.zsh_functions"
 }
