d964320f50e773e8d692c636a288328ac1bbf76a
Barinderpreet Singh <naffydharni006@gmail.com>
2025-02-13 12:19:54 +0530

    Enhance qutebrowser keymaps

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -14,3 +14,54 @@
 config.load_autoconfig()
 config.source('./themes/onedark.py')
 
+# Unbind existing movement keys
+config.unbind("h")
+config.unbind("j")
+config.unbind("k")
+config.unbind("l")
+
+# Rebind to new movement keys
+config.bind("j", "scroll left")
+config.bind("k", "scroll down")
+config.bind("l", "scroll up")
+config.bind(";", "scroll right")
+
+# Unbind existing keys for navigation
+config.unbind("H")
+config.unbind("L")
+config.unbind("J")
+config.unbind("K")
+
+# Rebind with new layout
+config.bind("J", "back")  # Instead of "H"
+config.bind(":", "forward")  # Instead of "L"
+config.bind("K", "tab-next")  # Instead of "J"
+config.bind("L", "tab-prev")  # Instead of "K"
+
+# Remap command input to ','
+config.bind(',', 'cmd-set-text :')
+
+# Carot mod keys
+config.unbind("h", mode="caret")
+config.unbind("j", mode="caret")
+config.unbind("k", mode="caret")
+config.unbind("l", mode="caret")
+
+config.bind("j", "move-to-prev-char", mode="caret")
+config.bind("k", "move-to-next-line", mode="caret")
+config.bind("l", "move-to-prev-line", mode="caret")
+config.bind(";", "move-to-next-char", mode="caret")
+
+
+# Bind Alt+c to copy selected text in normal and caret modes
+config.bind("<Alt-c>", "yank", mode="normal")
+config.bind("<Alt-c>", "yank", mode="caret")
+
+# Bind Alt+v to paste clipboard content in normal, caret, and insert modes
+config.bind("<Alt-v>", "insert-text {clipboard}", mode="insert")
+
+# Bind Alt+Backspace to delete previous word
+config.bind("<Alt-w>", "fake-key <Ctrl-Backspace>", mode="insert")
+
+# Bind Alt+Delete to delete next word
+config.bind("<Alt-d>", "fake-key <Ctrl-Delete>", mode="insert")
