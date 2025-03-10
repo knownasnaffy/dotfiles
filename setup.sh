109eeee1ea162136d197a72ae390defe1dd1d2e4
Barinderpreet Singh <naffydharni006@gmail.com>
2025-03-11 00:38:59 +0530

    Add xdotool for command line automation

--- setup.sh
+++ setup.sh
@@ -92,7 +92,7 @@ install_gh_ubuntu() {
 install_programs() {
     log "Detected Manjaro. Installing programs..."
     sudo pamac update --no-confirm
-    sudo pamac install --no-confirm zsh make gcc ripgrep unzip git xclip neovim fzf github-cli nvm fortune-mod base-devel fortune-mod cowsay fastfetch qutebrowser rofi polybar feh picom ttf-hack-nerd ghostty task maim brightnessctl pipewire pipewire-pulse pipewire-alsa wireplumber alsa-utils inotify-tools jq eva thefuck bat zoxide 7zip yazi zathura i3lock-color noto-fonts-emoji dunst
+    sudo pamac install --no-confirm zsh make gcc ripgrep unzip git xclip neovim fzf github-cli nvm fortune-mod base-devel fortune-mod cowsay fastfetch qutebrowser rofi polybar feh picom ttf-hack-nerd ghostty task maim brightnessctl pipewire pipewire-pulse pipewire-alsa wireplumber alsa-utils inotify-tools jq eva thefuck bat zoxide 7zip yazi zathura i3lock-color noto-fonts-emoji dunst xdotool
 
     install_oh_my_zsh
     install_neovim_config
