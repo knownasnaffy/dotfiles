7485835258345bcc6b7c007f936d4ec5d285b3e6
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2025-05-18 00:17:13 +0530

    Some changes

--- src/arch/paru.ts
+++ src/arch/paru.ts
@@ -1,25 +1,29 @@
-import { $, sleep } from "bun";
+import { $ } from "bun";
 import { existsSync } from "fs";
 import { commandExists } from "../utilities";
 import { Signale } from "signale";
 
 export default async function installParu() {
   const logger = new Signale({ interactive: true, scope: "Packages" });
-  logger.await("Checking paru installation");
-  await sleep(2000);
 
+  logger.await("Get sudo access");
+  await $`sudo -v`;
+
+  logger.await("Checking paru installation");
   const paruExists = await commandExists("paru");
 
   if (!paruExists) {
     logger.await("Paru not found, installing base packages");
-    await $`sudo pacman -Sy --noconfirm zsh make gcc ripgrep unzip git xclip neovim base-devel`;
+    await $`sudo pacman -Sy --noconfirm zsh make gcc ripgrep unzip git xclip neovim base-devel`.quiet();
 
     const clonePath = `${process.env.HOME}/code/clones/paru`;
 
     if (!existsSync(clonePath)) {
-      await $`git clone https://aur.archlinux.org/paru.git ${clonePath}`;
+      logger.await("Cloning paru");
+      await $`git clone https://aur.archlinux.org/paru.git ${clonePath}`.quiet();
     }
 
+    logger.await("Building paru");
     await $`bash -c 'cd ${clonePath} && makepkg -si --noconfirm'`;
   } else {
     logger.success("Paru already installed");
--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,9 @@ const distro: string | undefined = await detectDistro();
 const template = hasInteractiveFlag ? await determineTemplate() : "headless";
 console.log(template);
 
+//  TODO: Get passwordless access for sudo using something like
+//        `${username} ALL=(ALL) NOPASSWD: /usr/bin/pacman\n`
+//        by appending it to /etc/sudoers.d/temp-pacman-nopasswd
+//
 createDirs().catch(console.error);
 installPrograms(distro).catch(console.error);
