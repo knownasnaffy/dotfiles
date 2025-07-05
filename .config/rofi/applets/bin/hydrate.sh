8eee2ce6543c55dffc200e8ff5bec10697b64f0a
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2025-07-05 22:20:09 +0530

    Add an hourly hydrate reminder using rofi

---
+++ .config/rofi/applets/bin/hydrate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+yes=' Done'
+no=' Later'
+
+selected=$(
+    echo -e "$no\n$yes" | rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
+        -theme-str 'mainbox {orientation: vertical; children: [ "message", "listview" ];}' \
+        -theme-str 'listview {columns: 2; lines: 1;}' \
+        -theme-str 'element-text {horizontal-align: 0.5;}' \
+        -theme-str 'textbox {horizontal-align: 0.5;}' \
+        -dmenu \
+        -p 'Confirmation' \
+        -mesg 'Time to drink some water' \
+        -theme $HOME/.config/rofi/applets/type-1/style-2.rasi
+)
+if [[ $selected == $yes ]]; then
+    echo "Done"
+else
+    echo "Later"
+    sleep 300
+    exec $0
+fi
