ce092a846080ba6b822e2776f747f859bae555a4
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2026-01-22 06:05:10 +0530

    Configure global rofi dmenu style

--- .config/rofi/applets/bin/wifi.sh
+++ .config/rofi/applets/bin/wifi.sh
@@ -2,7 +2,7 @@
 
 # Rofi styles
 dir="$HOME/.config/rofi/launchers/type-1"
-theme='wifi'
+theme='dmenu'
 
 INTERFACE="wlan0"
 CACHE=/tmp/iwd_last_scan       # file stores Unix epoch of last scan
@@ -26,44 +26,44 @@ NETWORKS=$(iwctl station "$INTERFACE" get-networks | sed 's/\x1b\[[0-9;]*m//g' |
 SSID=$(echo "$NETWORKS" | rofi \
         -dmenu \
         -p "Connect to WiFi" \
-        -theme ${dir}/${theme}.rasi | awk '{
+    -theme ${dir}/${theme}.rasi | awk '{
         if ($1 == ">")
                 printf "> %s\n", $2;
         else
                 printf "- %s\n", $1;
 }')
 
-[ -z "$SSID" ] && exit 1
+    [ -z "$SSID" ] && exit 1
 
-if [[ "$SSID" == "> "* ]]; then
-    yes=' Yes'
-    no=' No'
+    if [[ "$SSID" == "> "* ]]; then
+        yes=' Yes'
+        no=' No'
 
-    selected=$(
-        echo -e "$no\n$yes" | rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
-            -theme-str 'mainbox {orientation: vertical; children: [ "message", "listview" ];}' \
-            -theme-str 'listview {columns: 2; lines: 1;}' \
-            -theme-str 'element-text {horizontal-align: 0.5;}' \
-            -theme-str 'textbox {horizontal-align: 0.5;}' \
-            -dmenu \
-            -p 'Confirmation' \
-            -mesg 'Disconnect?' \
-            -theme $HOME/.config/rofi/applets/type-1/style-2.rasi
-    )
-    if [[ $selected == $yes ]]; then
-        iwctl station $INTERFACE disconnect
-        echo "Disconnected"
-        exit 0
-    else
-        echo "Not disconnecting"
-        exit 0
+        selected=$(
+            echo -e "$no\n$yes" | rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
+                -theme-str 'mainbox {orientation: vertical; children: [ "message", "listview" ];}' \
+                -theme-str 'listview {columns: 2; lines: 1;}' \
+                -theme-str 'element-text {horizontal-align: 0.5;}' \
+                -theme-str 'textbox {horizontal-align: 0.5;}' \
+                -dmenu \
+                -p 'Confirmation' \
+                -mesg 'Disconnect?' \
+                -theme $HOME/.config/rofi/applets/type-1/style-2.rasi
+        )
+        if [[ $selected == $yes ]]; then
+            iwctl station $INTERFACE disconnect
+            echo "Disconnected"
+            exit 0
+        else
+            echo "Not disconnecting"
+            exit 0
+        fi
     fi
-fi
 
-if iwctl known-networks list | sed 's/\x1b\[[0-9;]*m//g' | awk '{ print ($1 == ">" ? $2 : $1) }' | grep -Fxq "$( echo $SSID  | cut -c3- | awk '{print $1}')"; then
-    iwctl station $INTERFACE connect $(echo $SSID | cut -c3- | awk '{print $1}')
-else
-    echo "Unrecognized: $(echo $SSID | cut -c3- | awk '{print $1}')"
-    PASS=$(rofi -dmenu -p "Password for $(echo $SSID | cut -c3- | awk '{print $1}')" -password -theme ${dir}/password.rasi)
-    iwctl --passphrase="$PASS" station "$INTERFACE" connect "$(echo $SSID | cut -c3- | awk '{print $1}')"
-fi
+    if iwctl known-networks list | sed 's/\x1b\[[0-9;]*m//g' | awk '{ print ($1 == ">" ? $2 : $1) }' | grep -Fxq "$( echo $SSID  | cut -c3- | awk '{print $1}')"; then
+        iwctl station $INTERFACE connect $(echo $SSID | cut -c3- | awk '{print $1}')
+    else
+        echo "Unrecognized: $(echo $SSID | cut -c3- | awk '{print $1}')"
+        PASS=$(rofi -dmenu -p "Password for $(echo $SSID | cut -c3- | awk '{print $1}')" -password -theme ${dir}/password.rasi)
+        iwctl --passphrase="$PASS" station "$INTERFACE" connect "$(echo $SSID | cut -c3- | awk '{print $1}')"
+    fi
