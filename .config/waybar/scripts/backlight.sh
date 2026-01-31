82e0375ffa5d86993f0fdb9441f4ef00c79d570b
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2026-01-31 08:19:46 +0530

    Update backlight script

--- .config/waybar/scripts/backlight.sh
+++ .config/waybar/scripts/backlight.sh
@@ -10,50 +10,42 @@
 # Date:    August 28, 2025
 # License: MIT
 
-VALUE=1
-
 usage() {
-	cat <<- EOF
+    cat <<- EOF
 		USAGE: ${0##*/} [OPTIONS]
 
 		Adjust screen brightness and send a notification with the current level
 
 		OPTIONS:
-		    up   <value>    Increase brightness by <value>
-		    down <value>    Decrease brightness by <value>
-		                        Default value: $VALUE
+		    up                  Increase brightness
+		    down                Decrease brightness
 
 		EXAMPLES:
 		    Increase brightness:
 		        $ ${0##*/} up
 
-		    Decrease brightness by 5:
-		        $ ${0##*/} down 5
+		    Decrease brightness:
+		        $ ${0##*/} down
 	EOF
-	exit 1
+    exit 1
 }
 
 main() {
-	local action=$1
-	local value=${2:-$VALUE}
+    local action=$1
 
-	((value > 0)) || usage
+    case $action in
+        "up" | "down")
+            case $action in
+                "up") swayosd-client --brightness raise > /dev/null ;;
+                "down") swayosd-client --brightness lower > /dev/null ;;
+            esac
 
-	case $action in
-		"up" | "down")
-			case $action in
-				"up") brightnessctl -n set "${value}%+" > /dev/null ;;
-				"down") brightnessctl -n set "${value}%-" > /dev/null ;;
-			esac
+            local level
+            level=$(brightnessctl -m | awk -F "," '{print $4}')
 
-			local level
-			level=$(brightnessctl -m | awk -F "," '{print $4}')
-
-			notify-send "Brightness: $level" -h int:value:"$level" -i \
-				"contrast" -h string:x-canonical-private-synchronous:backlight
-			;;
-		*) usage ;;
-	esac
+            ;;
+        *) usage ;;
+    esac
 }
 
 main "$@"
