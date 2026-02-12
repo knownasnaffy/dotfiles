d66159652be540b6dfd1856cb902a0d00e3e35ca
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2026-02-12 13:45:48 +0530

    Update folder rules logic in yazi to be reusable

--- .config/yazi/plugins/folder-rules.yazi/main.lua
+++ .config/yazi/plugins/folder-rules.yazi/main.lua
@@ -1,19 +1,23 @@
-local special_dirs = { "Downloads", "Videos", "Screenshots", "Screen Recordings", "Beeper" }
+local function ends_with_any(str, list)
+    for _, v in ipairs(list) do
+        if str:ends_with(v) then
+            return true
+        end
+    end
+    return false
+end
 
 local function setup()
-	ps.sub("ind-sort", function(opt)
-		local cwd = cx.active.current.cwd
+    ps.sub("ind-sort", function(opt)
+        local cwd = cx.active.current.cwd
 
-		for _, dir in ipairs(special_dirs) do
-			if cwd:ends_with(dir) then
-				opt.by, opt.reverse, opt.dir_first = "mtime", true, false
-				return opt
-			end
-		end
-
-		opt.by, opt.reverse, opt.dir_first = "natural", false, true
-		return opt
-	end)
+        if ends_with_any(cwd, { "Downloads", "Videos", "Screenshots", "Screen Recordings", "Beeper" }) then
+            opt.by, opt.reverse, opt.dir_first = "mtime", true, false
+        else
+            opt.by, opt.reverse, opt.dir_first = "natural", false, true
+        end
+        return opt
+    end)
 end
 
 return { setup = setup }
